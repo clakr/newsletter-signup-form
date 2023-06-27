cc39f530fae5ad96abf61616c08fb4060a8f6354
clakr <clarktolosa@gmail.com>
2023-06-28 07:29:29 +0800

    added `globals.css` and `prettier` plugin for `eslint`

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import "./_preflight.css";
 import authorModal from "./_authorModal";
 import projectConfig from "./_projectConfig";
+import "./_preflight.css";
+import "./globals.css";
 
 createApp(App).use(projectConfig).use(authorModal).mount("#app");
